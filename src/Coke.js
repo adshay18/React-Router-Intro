bc3a3fddb6127961b659f10d4acdd0a4876074b9
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-06 17:02:24 -0400

    style coke page

--- src/Coke.js
+++ src/Coke.js
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom';
+import './Coke.css';
 
 const Coke = () => {
 	return (
 		<div className="Coke">
-			<p>Here's some coke</p>
-			<Link exact to="/">
-				Pick something else
-			</Link>
+			<div className="Coke-text">
+				<h1>Here's some Coke</h1>
+				<p>Wow it's so delicious...</p>
+				<br />
+				<Link exact to="/">
+					Pick something else
+				</Link>
+			</div>
+			<div className="Coke-img" />
 		</div>
 	);
 };
