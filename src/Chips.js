bbf9dfc28240ad4a3bb2f07472109aebe245c1d6
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-06 17:12:25 -0400

    style chips page

--- src/Chips.js
+++ src/Chips.js
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom';
+import './Chips.css';
 
 const Chips = () => {
 	return (
 		<div className="Chips">
-			<p>Here's some chips</p>
-			<Link exact to="/">
-				Pick something else
-			</Link>
+			<div className="Chips-text">
+				<h1>Here's some Lays Chips</h1>
+				<p>So salty, could use a Coke...</p>
+				<br />
+				<Link exact to="/">
+					Pick something else
+				</Link>
+			</div>
+			<img src="https://www.clipartmax.com/png/full/108-1082643_strike-and-spare-hendersonville-lays-potato-chips-classic-4-25-oz.png" />
 		</div>
 	);
 };
