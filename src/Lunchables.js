739f1958f017a75c593791d8ff77650adc0543ae
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-06 17:18:28 -0400

    finish styling

--- src/Lunchables.js
+++ src/Lunchables.js
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom';
+import './Lunchables.css';
 
 const Lunchables = () => {
 	return (
 		<div className="Lunchables">
-			<p>Here's some Lunchables, the cool ones with the capri sun</p>
-			<Link exact to="/">
-				Pick something else
-			</Link>
+			<div className="Lunchables-text">
+				<h1>Here's some Lunchables</h1>
+				<p>Finally picked the right snack.</p>
+				<br />
+				<Link exact to="/">
+					Pick something else
+				</Link>
+			</div>
 		</div>
 	);
 };
